75241e485399ed34c05237c583c55dba375cbce2
KaavyaMa <kaavyamanam@seattleacademy.org>
2024-05-22 09:15:39 -0700

    Lorax Website

--- Project 14 - Business Website/example/app.js
+++ Project 14 - Business Website/example/app.js
@@ -4,17 +4,17 @@
 function secretMessage() {
     alert("You have unlocked the secret message!")
 }
-function displayDog() {
-    document.getElementById("dogContent").style.display = "block";
-    document.getElementById("catContent").style.display = "none";
+function displayLorax() {
+    document.getElementById("loraxContent").style.display = "block";
+    document.getElementById("loraxxContent").style.display = "none";
     document.getElementById("defaultContent").style.display = "none";
 }
-function displayCat() {
-    document.getElementById("dogContent").style.display = "none";
-    document.getElementById("catContent").style.display = "block";
+function displayLoraxx() {
+    document.getElementById("loraxContent").style.display = "none";
+    document.getElementById("loraxxContent").style.display = "block";
     document.getElementById("defaultContent").style.display = "none";
 }
-function redBackground() {
-    document.body.style.backgroundColor = "red";
+function orangeBackground() {
+    document.body.style.backgroundColor = "orange";
     console.log(document)
 }
